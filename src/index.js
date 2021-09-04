701dc4c5dc89e71bdaab743a1a80694c025c410b
Joao Lopes <lopis@users.noreply.github.com>
2021-09-04 23:38:18 +0200

    improve movement

--- src/index.js
+++ src/index.js
@@ -7,49 +7,82 @@ let { canvas } = init();
 canvas.height = innerHeight
 canvas.width = innerWidth
 
-let sprite = Sprite({
-  x: innerWidth / 2,        // starting x,y position of the sprite
-  y: innerHeight / 2,
-  color: 'black',  // fill color of the sprite rectangle
-  width: 20,     // width and height of the sprite rectangle
-  height: 20,
-  angle: 0, //360 * Math.random(),
-});
+let ants = [];
 
-const chance = 0.5
-const change = 30
-const speed = 3
+const spriteSize = 5
+const createAnt = () => {
+  ants.push(Sprite({
+    x: innerWidth / 2,        // starting x,y position of the sprite
+    y: innerHeight / 2,
+    color: 'black',  // fill color of the sprite rectangle
+    width: spriteSize,     // width and height of the sprite rectangle
+    height: spriteSize,
+    angle: 360 * Math.random(),
+  }))
+}
+const chance = 0.2
+const change = 45
+const speed = 1
+const padding = 10
+
+const updateDirection = (sprite, chance) => {
+  if (Math.random() < chance) {
+    sprite.angle = sprite.angle + (Math.random() * change * 2 - change)
+    sprite.dx = speed * Math.cos(degToRad(sprite.angle))
+    sprite.dy = speed * Math.sin(degToRad(sprite.angle))
+  }
+}
+
+const checkBoundaries = (sprite) => {
+  if (!sprite.isBouncing) {
+    if (sprite.x > canvas.width - padding || sprite.x < padding) {
+      sprite.angle = 180 - sprite.angle
+      sprite.isBouncing = true
+      updateDirection(sprite, 1)
+    } else if (sprite.y > canvas.height - padding || sprite.y < padding) {
+      sprite.angle = -sprite.angle
+      sprite.isBouncing = true
+      updateDirection(sprite, 1)
+    }
+  }
+
+  if (
+    sprite.isBouncing && (
+      sprite.x < canvas.width - padding
+      && sprite.y < canvas.height - padding
+      && sprite.y > padding
+      && sprite.x > padding
+    )
+  ) {
+    sprite.isBouncing = false
+  }
+}
+
+
+createAnt()
+createAnt()
+createAnt()
+createAnt()
+createAnt()
+createAnt()
+createAnt()
+createAnt()
+createAnt()
+
 let loop = GameLoop({  // create the main game loop
   update: function () { // update the game state
-    if (Math.random() < chance) {
-      if (!sprite.isBouncing) {
-        if (sprite.x > canvas.width - sprite.width - 10 || sprite.x < 10) {
-          sprite.angle = 180 - sprite.angle;
-        } else if (sprite.y > canvas.height - sprite.height - 10 || sprite.y < 10) {
-          sprite.angle = -sprite.angle
-        }
-        sprite.isBouncing = true
-      }
-
-      if (
-        sprite.isBouncing && (
-          sprite.x < canvas.width - sprite.width - 100
-          || sprite.y < canvas.height - sprite.height - 100
-          || sprite.y > 100
-          || sprite.x > 100
-        )
-      ) {
-        sprite.isBouncing = false
-      }
-
-      sprite.angle = sprite.angle + (Math.random() * change * 2 - change)
-      sprite.dx = speed * Math.cos(degToRad(sprite.angle))
-      sprite.dy = speed * Math.sin(degToRad(sprite.angle))
-    }
-    sprite.update();
+    ants.forEach(ant => {
+      checkBoundaries(ant)
+      updateDirection(ant, chance)
+      ant.update();
+    })
   },
   render: function () { // render the game state
-    sprite.render();
+    ants.forEach(ant => {
+      checkBoundaries(ant)
+      updateDirection(ant, chance)
+      ant.render();
+    })
   }
 });
 
